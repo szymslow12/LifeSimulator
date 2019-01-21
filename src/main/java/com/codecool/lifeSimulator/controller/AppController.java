2882313c4c2bff98382b9ba9f74d03037516f025
szymslow12 <szym.slow12@gmail.com>
2019-01-21 12:54:32 +0100

    add AppController with field Planet and run()

---
+++ src/main/java/com/codecool/lifeSimulator/controller/AppController.java
@@ -0,0 +1,11 @@
+package com.codecool.lifeSimulator.controller;
+
+import com.codecool.lifeSimulator.model.Planet;
+
+public class AppController {
+    private Planet planet;
+
+    public void run() {
+        System.out.println(planet);
+    }
+}
