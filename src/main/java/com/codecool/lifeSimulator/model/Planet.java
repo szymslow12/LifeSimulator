d6a9fe0d13875c55cc2158be1a69106f0efef130
szymslow12 <szym.slow12@gmail.com>
2019-01-21 13:06:15 +0100

    add generatePlanetState()

--- src/main/java/com/codecool/lifeSimulator/model/Planet.java
+++ src/main/java/com/codecool/lifeSimulator/model/Planet.java
@@ -1,5 +1,7 @@
 package com.codecool.lifeSimulator.model;
 
+import java.util.stream.IntStream;
+
 public class Planet {
     private Square[][] planetState;
 
@@ -9,5 +11,14 @@ public class Planet {
 
     public Planet(int planetHight, int planetWidth) {
         planetState = new Square[planetHight][planetWidth];
+    }
+
+
+    public void generatePlanetState() {
+        IntStream.range(0, planetState.length).forEach(
+            height -> IntStream.range(0, planetState[height].length).forEach(
+                width -> planetState[height][width] = new Square()
+            )
+        );
     }
 }
