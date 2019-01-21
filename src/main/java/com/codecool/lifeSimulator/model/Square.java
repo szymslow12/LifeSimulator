3afa76234688823ad32715cbe1066c2fe29e6308
szymslow12 <szym.slow12@gmail.com>
2019-01-21 12:59:33 +0100

    add Square with fields String name, int energy and getters

--- src/main/java/com/codecool/lifeSimulator/model/Square.java
+++ src/main/java/com/codecool/lifeSimulator/model/Square.java
@@ -1,4 +1,16 @@
 package com.codecool.lifeSimulator.model;
 
 public class Square {
+
+    private String name;
+    private int energy;
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getEnergy() {
+        return energy;
+    }
 }
