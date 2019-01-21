cefc00d7df0684db37baa41c2d1a74c849c6d217
szymslow12 <szym.slow12@gmail.com>
2019-01-21 13:40:11 +0100

    change AppController->extends Pane,add constructor which sets background

--- src/main/java/com/codecool/lifeSimulator/controller/AppController.java
+++ src/main/java/com/codecool/lifeSimulator/controller/AppController.java
@@ -1,9 +1,19 @@
 package com.codecool.lifeSimulator.controller;
 
 import com.codecool.lifeSimulator.model.Planet;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-public class AppController {
+public class AppController extends Pane {
     private Planet planet;
+
+    public AppController() {
+        setBackground(new Background(new BackgroundFill(Color.web("#ff9999"), CornerRadii.EMPTY, Insets.EMPTY)));
+    }
 
     public void run() {
         System.out.println(planet);
