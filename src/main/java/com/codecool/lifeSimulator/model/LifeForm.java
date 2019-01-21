afbb5526aa085193cb0d9ff771d043a422c1d2dd
Riczard <riczardkiller@gmail.com>
2019-01-21 19:33:29 +0100

    add skeleton for LifeForm and simple interface

---
+++ src/main/java/com/codecool/lifeSimulator/model/LifeForm.java
@@ -0,0 +1,13 @@
+package com.codecool.lifeSimulator.model;
+
+public class LifeForm extends Square implements Moveable{
+
+    public void move() {
+        //TODO
+    }
+
+    public void splitIntoToForm() {
+        //TODO
+    }
+
+}
