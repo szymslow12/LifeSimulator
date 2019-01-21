b84af34f364be18bcd47d6f6dd52914ff2cb7a6e
szymslow12 <szym.slow12@gmail.com>
2019-01-21 22:08:48 +0100

    change for test addPlanetStateToScene

--- src/main/java/com/codecool/lifeSimulator/controller/AppController.java
+++ src/main/java/com/codecool/lifeSimulator/controller/AppController.java
@@ -1,12 +1,10 @@
 package com.codecool.lifeSimulator.controller;
 
 import com.codecool.lifeSimulator.model.Planet;
+import com.codecool.lifeSimulator.model.Position;
 import com.codecool.lifeSimulator.model.Square;
 import javafx.geometry.Insets;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 import java.util.stream.Stream;
@@ -19,7 +17,6 @@ public class AppController extends Pane {
         planet.generatePlanetState();
         setBackground(new Background(new BackgroundFill(Color.web("#ffffff"), CornerRadii.EMPTY, Insets.EMPTY)));
         addPlanetStateToScene();
-//        getStyleClass().add("square-container");
     }
 
     public void run() {
@@ -29,6 +26,17 @@ public class AppController extends Pane {
 
     private void addPlanetStateToScene() {
         Square[][] planetState = planet.getPlanetState();
-        Stream.of(planetState).forEach(line -> Stream.of(line).forEach(square -> getChildren().add(square.getSquareCanvas())));
+        //for tests
+        planetState[0][4] = new Square("FOOD", 5);
+        planetState[0][4].setPosition(new Position(4, 0));
+        planetState[4][1] = new Square("FOOD", 5);
+        planetState[4][1].setPosition(new Position(1, 4));
+        planetState[4][4] = new Square("LIFE", 100);
+        planetState[4][4].setPosition(new Position(4, 4));
+        Stream.of(planetState).forEach(
+            line -> Stream.of(line).forEach(
+                square -> getChildren().add(square.getSquareCanvas())
+            )
+        );
     }
 }
