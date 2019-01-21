b5308a7e61c5a037ea1a411c061d1b7e1d9cafdf
szymslow12 <szym.slow12@gmail.com>
2019-01-21 13:39:23 +0100

    change Main ->extends Application, add start(), change main()

--- src/main/java/com/codecool/lifeSimulator/Main.java
+++ src/main/java/com/codecool/lifeSimulator/Main.java
@@ -1,8 +1,22 @@
 package com.codecool.lifeSimulator;
 
-public class Main {
+import com.codecool.lifeSimulator.controller.AppController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello !");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        AppController controller = new AppController();
+        primaryStage.setFullScreen(true);
+        primaryStage.setTitle("Life Simulator");
+        primaryStage.setScene(new Scene(controller));
+        primaryStage.show();
     }
 }
