28f8c2a610470599cfc9823c46d0d3fc99ae4e4e
szymslow12 <szym.slow12@gmail.com>
2019-01-22 15:12:48 +0100

    add LifeFormSquare with impl drawShape, remove drawShape from LifeForm

--- src/main/java/com/codecool/lifeSimulator/model/LifeForm.java
+++ src/main/java/com/codecool/lifeSimulator/model/LifeForm.java
@@ -1,27 +1,11 @@
 package com.codecool.lifeSimulator.model;
 
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-
 public class LifeForm extends Square {
 
     private Movements movements = new Movements();
   
     public LifeForm(int posX, int poxY) {
         super("LIFE_FORM", 100, posX, poxY);
-    }
-
-
-    @Override
-    void drawShape(GraphicsContext context, float squareSide, float translatePos) {
-        context.strokeRect(0, 0, squareSide, squareSide);
-        float lifePos = squareSide / 4;
-        context.setFill(Color.web("#ff0000"));
-        context.fillOval(lifePos, lifePos, 50,50);
-        context.setFill(Color.web("#000000"));
-        context.setFont(new Font(10));
-        context.fillText("Energy: " + getEnergy(), translatePos, translatePos * 6);
     }
 
     public void move(Position position) {
---
+++ src/main/java/com/codecool/lifeSimulator/view/LifeFormSquare.java
@@ -0,0 +1,24 @@
+package com.codecool.lifeSimulator.view;
+
+import com.codecool.lifeSimulator.model.Square;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class LifeFormSquare extends SquareView {
+
+    public LifeFormSquare(Square square) {
+        super(square);
+    }
+
+    @Override
+    void drawShape(GraphicsContext context, float squareSide, float translatePos) {
+        context.strokeRect(0, 0, squareSide, squareSide);
+        float lifePos = squareSide / 4;
+        context.setFill(Color.web("#ff0000"));
+        context.fillOval(lifePos, lifePos, 50,50);
+        context.setFill(Color.web("#000000"));
+        context.setFont(new Font(10));
+        context.fillText("Energy: " + getSquare().getEnergy(), translatePos, translatePos * 6);
+    }
+}
