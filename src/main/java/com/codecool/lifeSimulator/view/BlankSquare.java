930734bc5dc5e23a738478b2b3d887ec779a10a5
szymslow12 <szym.slow12@gmail.com>
2019-01-22 15:10:12 +0100

    add BlankSquare with impl drawShape

---
+++ src/main/java/com/codecool/lifeSimulator/view/BlankSquare.java
@@ -0,0 +1,17 @@
+package com.codecool.lifeSimulator.view;
+
+import com.codecool.lifeSimulator.model.Square;
+import javafx.scene.canvas.GraphicsContext;
+
+public class BlankSquare extends SquareView {
+
+    public BlankSquare(Square square) {
+        super(square);
+    }
+
+
+    @Override
+    void drawShape(GraphicsContext context, float squareSide, float translatePos) {
+        context.strokeRect(0, 0, squareSide, squareSide);
+    }
+}
