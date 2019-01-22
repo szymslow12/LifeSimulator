061a77f0cb7d7c6d587a514cdb3cf1df20ed7ee7
szymslow12 <szym.slow12@gmail.com>
2019-01-22 09:40:50 +0100

    change drawShape -> if life then writes life energy too

--- src/main/java/com/codecool/lifeSimulator/model/Square.java
+++ src/main/java/com/codecool/lifeSimulator/model/Square.java
@@ -4,6 +4,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class Square {
 
@@ -71,6 +72,9 @@ public class Square {
             float lifePos = squareSide / 4;
             context.setFill(Color.web("#ff0000"));
             context.fillOval(lifePos, lifePos, 50,50);
+            context.setFill(Color.web("#000000"));
+            context.setFont(new Font(10));
+            context.fillText("Energy: " + energy, translatePos, translatePos * 6);
         }
     }
 
