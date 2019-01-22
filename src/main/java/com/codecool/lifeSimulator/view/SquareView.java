a81961972af7fd7705c12cf59e8a290d031a26f1
szymslow12 <szym.slow12@gmail.com>
2019-01-22 15:23:05 +0100

    add static renderSquare, make proper fixes -> not creates new Canvas but draw itself

--- src/main/java/com/codecool/lifeSimulator/view/SquareView.java
+++ src/main/java/com/codecool/lifeSimulator/view/SquareView.java
@@ -10,31 +10,45 @@ public abstract class SquareView extends Canvas {
     private Square square;
 
     public SquareView(Square square) {
+        super(100, 100);
         this.square = square;
+        renderSquare();
     }
 
     public Square getSquare() {
         return square;
     }
 
-    private Canvas getSquareCanvas() {
-        Canvas canvas = new Canvas(100, 100);
-        GraphicsContext context = canvas.getGraphicsContext2D();
+    private void renderSquare() {
+        GraphicsContext context = getGraphicsContext2D();
         // 96 because square has 4 sides and each side has line with 1px size
         drawShape(context, 96, 1.5f);
-        setCanvasProperties(canvas);
-        return canvas;
+        setCanvasProperties();
     }
 
 
-    private void setCanvasProperties(Canvas canvas) {
+    private void setCanvasProperties() {
         float canvasX = square.getPosition().getX() * 100;
         float canvasY = square.getPosition().getY() * 100;
-        canvas.setLayoutX(canvasX + 10);
-        canvas.setLayoutY(canvasY + 10);
-        canvas.setEffect(new DropShadow(15, Color.web("#000000")));
+        setLayoutX(canvasX + 10);
+        setLayoutY(canvasY + 10);
+        setEffect(new DropShadow(15, Color.web("#000000")));
     }
 
 
     abstract void drawShape(GraphicsContext context, float squareSide, float translatePos);
+
+
+    public static SquareView renderSquare(Square square) {
+        switch (square.getName()) {
+            case "BLANK":
+                return new BlankSquare(square);
+            case "FOOD":
+                return new FoodSquare(square);
+            case "LIFE_FORM":
+                return new LifeFormSquare(square);
+            default:
+                return new BlankSquare(square);
+        }
+    }
 }
