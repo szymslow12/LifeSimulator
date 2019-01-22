9d0f0f3978496b45a583404274469981661753d4
szymslow12 <szym.slow12@gmail.com>
2019-01-22 15:13:58 +0100

    add FoodSquare with impl drawShape, remove drawShape from Food

--- src/main/java/com/codecool/lifeSimulator/model/Food.java
+++ src/main/java/com/codecool/lifeSimulator/model/Food.java
@@ -1,20 +1,8 @@
 package com.codecool.lifeSimulator.model;
 
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-
 public class Food extends Square {
 
     public Food(int posX, int poxY) {
         super("FOOD", 5, posX, poxY);
-    }
-
-
-    @Override
-    void drawShape(GraphicsContext context, float squareSide, float translatePos) {
-        float squareFillSize = squareSide - translatePos - 0.5f;
-        context.strokeRect(0, 0, squareSide, squareSide);
-        context.setFill(Color.web("#00e64d"));
-        context.fillRect(translatePos,translatePos, squareFillSize, squareFillSize);
     }
 }
