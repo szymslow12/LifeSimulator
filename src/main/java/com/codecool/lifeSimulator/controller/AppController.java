d0856b7787e058efbea558387fda581ccbbc97f3
szymslow12 <szym.slow12@gmail.com>
2019-01-23 15:09:58 +0100

    add generateFoodOnRandomPosition(), remove everything with shufflePlanetState

--- src/main/java/com/codecool/lifeSimulator/controller/AppController.java
+++ src/main/java/com/codecool/lifeSimulator/controller/AppController.java
@@ -6,10 +6,6 @@ import javafx.geometry.Insets;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
-import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 public class AppController extends Pane{
     private Planet planet;
     private PlanetRender render;
@@ -36,8 +32,7 @@ public class AppController extends Pane{
 
     private void runThreads() {
         new MapUpdater(this).start();
-//        new MapShuffle(this).start();
-        new FoodGenerator(planet, render).start();
+        new FoodGenerator(this).start();
     }
 
   
@@ -46,50 +41,28 @@ public class AppController extends Pane{
     }
 
 
-    public synchronized void updatePlanetState() throws InterruptedException {
-        if (!render.getFlag()) {
+    public synchronized void generateFoodOnRandomPosition() throws InterruptedException {
+        if (render.getFlag()) {
             System.out.println(Thread.currentThread().getName() + " waits");
             wait();
         }
-        System.out.println(Thread.currentThread().getName() + " started updating map!");
-        render.update(planet.getPlanetState(), this);
-        render.setFlag(false);
-        System.out.println("Map updated!");
-        notify();
-    }
-
-
-    public synchronized void shufflePlanetState() throws InterruptedException {
-        if (render.getFlag()) {
-            wait();
-        }
-        AppController.shufflePositions(planet.getPlanetState());
+        Thread.sleep(1000);
+        planet.generateFoodOnRandomPosition();
         render.setFlag(true);
         notify();
     }
 
 
-    private static void shufflePositions(Square[][] array)
-    {
-        Random random = new Random();
-        List<Position> positions = new LinkedList<Position>() {
-            private void addPositions() {
-                IntStream.range(0, 51).forEach(y -> IntStream.range(0, 92).forEach(x -> add(new Position(x, y))));
-                Collections.shuffle(this);
-            }
-
-            {
-                addPositions();
-            }
-        };
-        Stream.of(array).forEach(line -> Stream.of(line).forEach(square -> changePosition(square, positions, random)));
-    }
-
-    private static void changePosition(Square square, List<Position> positions, Random random) {
-        int index = random.nextInt(positions.size());
-        Position position = positions.get(index);
-        positions.remove(index);
-        square.setPosition(position);
-
+    public synchronized void updatePlanetState() throws InterruptedException {
+        if (!render.getFlag()) {
+            System.out.println(Thread.currentThread().getName() + " waits");
+            wait();
+        }
+        Thread.sleep(1000);
+        System.out.println(Thread.currentThread().getName() + " started updating map!");
+        render.update(planet.getPlanetState(), this);
+        render.setFlag(false);
+        System.out.println("Map updated!");
+        notify();
     }
 }
