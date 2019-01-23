e6fce5ece1bd6981ba808de4ac2816f3b7328f1a
szymslow12 <szym.slow12@gmail.com>
2019-01-23 15:17:30 +0100

    refactor generateFoodOnRandomPosition -> add generateFood

--- src/main/java/com/codecool/lifeSimulator/model/Planet.java
+++ src/main/java/com/codecool/lifeSimulator/model/Planet.java
@@ -23,18 +23,27 @@ public class Planet {
 
 
     public void generateFoodOnRandomPosition() {
+        Random random = new Random();
         boolean isGenerated= false;
         System.out.println(Thread.currentThread().getName() + " started generating food!");
         while (!isGenerated) {
-            Random random = new Random();
             int randomPosX = random.nextInt(planetState[0].length);
             int randomPosY = random.nextInt(planetState.length);
             Square square = planetState[randomPosY][randomPosX];
-            if (square.getName().equals("BLANK")) {
-                System.out.println(String.format("Food generated on positions x=%s, y=%s", randomPosX, randomPosY));
-                planetState[randomPosY][randomPosX] = new Food(randomPosX, randomPosY);
-                isGenerated = true;
-            }
+            isGenerated = generateFood(square);
         }
+    }
+
+
+    private boolean generateFood(Square square) {
+        Position position = square.getPosition();
+        int x = position.getX();
+        int y = position.getY();
+        if (square.getName().equals("BLANK")) {
+            System.out.println(String.format("Food generated on positions x=%s, y=%s", x, y));
+            planetState[y][x] = new Food(x, y);
+            return true;
+        }
+        return false;
     }
 }
