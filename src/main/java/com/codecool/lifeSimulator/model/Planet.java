9d55fe63ee1fe498a9efa0138cc936ec33634590
szymslow12 <szym.slow12@gmail.com>
2019-01-23 13:13:23 +0100

    add getLifeForms, addToList

--- src/main/java/com/codecool/lifeSimulator/model/Planet.java
+++ src/main/java/com/codecool/lifeSimulator/model/Planet.java
@@ -1,6 +1,9 @@
 package com.codecool.lifeSimulator.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Planet {
     private Square[][] planetState;
@@ -25,5 +28,23 @@ public class Planet {
 
     private Square generateSquare(int posX, int posY) {
         return new Blank(posX, posY);
+    }
+
+
+    public List<LifeForm> getLifeForms() {
+        List<LifeForm> lifeForms = new ArrayList<>();
+        Stream.of(planetState).forEach(
+            squareLine -> Stream.of(squareLine).forEach(
+                square -> addToList(square, lifeForms)
+            )
+        );
+        return lifeForms;
+    }
+
+
+    private void addToList(Square square, List<LifeForm> lifeForms) {
+        if (square.getName().equals("LIFE_FORM")) {
+            lifeForms.add((LifeForm) square);
+        }
     }
 }
