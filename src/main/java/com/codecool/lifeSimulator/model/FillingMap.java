b40c5357bb274ce97cf2743f1d68c0b57bffeabd
Riczard <riczardkiller@gmail.com>
2019-01-23 12:55:10 +0100

    add filling map class

---
+++ src/main/java/com/codecool/lifeSimulator/model/FillingMap.java
@@ -0,0 +1,77 @@
+package com.codecool.lifeSimulator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FillingMap {
+
+    private int heightMap;
+    private int widthMap;
+    private Square[][] planetState;
+    private List<Position> usedPosition = new ArrayList<>();
+
+    public FillingMap(int heightMap, int widthMap, Square[][] planetState) {
+        this.heightMap = heightMap;
+        this.widthMap = widthMap;
+        this.planetState = planetState;
+    }
+
+    public void fillMap(int percentOfMap, int count) {
+        if (checkFillingProportions(percentOfMap, count)) {
+            fillMapWithFood(percentOfMap);
+            fillMapWithLifeForm(count);
+        }
+        throw new IllegalArgumentException("Wrong proportions");
+    }
+
+    private boolean checkFillingProportions(int percentOfMap, int count) {
+        int area = heightMap * widthMap;
+        int areaLeft = area - (area * percentOfMap) - count;
+        return area > areaLeft;
+    }
+
+
+    public void fillMapWithFood(int percentOfMap) {
+        int numberSquareToFill = heightMap * percentOfMap;
+
+        while (numberSquareToFill > 0) {
+            Position position = generateUniquePosition();
+            usedPosition.add(position);
+            int posY = position.getY();
+            int posX = position.getX();
+            planetState[posY][posX] = new Food(posX, posY);
+            numberSquareToFill--;
+        }
+    }
+
+    private Position generateUniquePosition() {
+        Position position;
+        do {
+            position = generateRandomPos();
+        } while (checkPositionIsNotUsed(position));
+
+        return position;
+    }
+
+    private Position generateRandomPos() {
+        int randomY = ThreadLocalRandom.current().nextInt(0, heightMap + 1);
+        int randomX = ThreadLocalRandom.current().nextInt(0, widthMap + 1);
+        return new Position(randomX, randomY);
+    }
+
+    private boolean checkPositionIsNotUsed(Position position) {
+        return usedPosition.contains(position);
+    }
+
+    public void fillMapWithLifeForm(int count) {
+        for (int i = 0; i < count; i++) {
+            Position position = generateUniquePosition();
+            usedPosition.add(position);
+            int posY = position.getY();
+            int posX = position.getX();
+            planetState[posY][posX] = new LifeForm(posX, posY);
+        }
+    }
+
+}
