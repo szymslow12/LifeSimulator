0c9fd47b44fa9779bad79b2536649db92b293d72
szymslow12 <szym.slow12@gmail.com>
2019-01-24 13:00:24 +0100

    change AppController -> not extends Pane,add field PlanetView and getter

--- src/main/java/com/codecool/lifeSimulator/controller/AppController.java
+++ src/main/java/com/codecool/lifeSimulator/controller/AppController.java
@@ -3,19 +3,15 @@ package com.codecool.lifeSimulator.controller;
 import com.codecool.lifeSimulator.model.FoodGenerator;
 import com.codecool.lifeSimulator.model.Planet;
 import com.codecool.lifeSimulator.view.PlanetRender;
-import javafx.geometry.Insets;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import com.codecool.lifeSimulator.view.PlanetView;
 
-public class AppController extends Pane {
+public class AppController {
     private Planet planet;
+    private PlanetView planetView;
 
     public AppController() {
-        setBackground(new Background(new BackgroundFill(Color.web("#ffffff"), CornerRadii.EMPTY, Insets.EMPTY)));
         planet = new Planet(51, 92);
+        planetView = new PlanetView(planet);
         runThreads();
     }
 
@@ -27,6 +23,11 @@ public class AppController extends Pane {
 
     public synchronized Planet getPlanet() {
         return planet;
+    }
+
+
+    public PlanetView getPlanetView() {
+        return planetView;
     }
 
 }
