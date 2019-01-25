4502000c157ac2b94010ca0757df89e5f3f02135
szymslow12 <szym.slow12@gmail.com>
2019-01-25 09:00:25 +0100

    change Thread.sleep to 2seconds

--- src/main/java/com/codecool/lifeSimulator/model/FoodGenerator.java
+++ src/main/java/com/codecool/lifeSimulator/model/FoodGenerator.java
@@ -47,7 +47,7 @@ public class FoodGenerator implements Runnable {
         while (true) {
             generateFoodOnRandomPosition();
             try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
